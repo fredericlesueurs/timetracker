5496ddca9e17cbb09ac9d6c9ace774f296d8226f
Frédéric Lesueurs <frederic.lesueurs@gmail.com>
2023-05-19 17:19:43 +0200

    Fix session list order

--- app/Filament/Resources/SessionResource/Pages/ListSessions.php
+++ app/Filament/Resources/SessionResource/Pages/ListSessions.php
@@ -5,10 +5,16 @@ namespace App\Filament\Resources\SessionResource\Pages;
 use App\Filament\Resources\SessionResource;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListSessions extends ListRecords
 {
     protected static string $resource = SessionResource::class;
+
+    protected function getTableQuery(): Builder
+    {
+        return parent::getTableQuery()->orderBy('created_at', 'desc');
+    }
 
     protected function getActions(): array
     {
